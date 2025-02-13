3e02bc604530f3b78921f1032e66e44b8df3a41e
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-02-13 09:25:05 -0300

    Testing class

--- MonthlyHoursTracking/TestEmployee.py
+++ MonthlyHoursTracking/TestEmployee.py
@@ -1,1 +1,12 @@
 from Employee import *
+
+employee = Employee('Matheus', 'matheus@email.com')
+
+employee.hours_record('Jan',300)
+employee.hours_record('Feb', 200)
+employee.hourly_salary_record('Jan', 30)
+employee.hourly_salary_record('Feb', 30)
+
+print(employee)
+print(f'Total salary January: {employee.calculate_salary('Jan')}')
+print(f'Total salary February: {employee.calculate_salary('Feb')}')
